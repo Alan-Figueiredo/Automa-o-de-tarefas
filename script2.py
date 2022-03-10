c70ec12f5b5ae8cd74976c2b244c82199e129b6a
PyAlan <98132046+PyAlan@users.noreply.github.com>
2022-03-09 22:51:55 -0300

    Update script2.py

--- script2.py
+++ script2.py
@@ -2,28 +2,28 @@ import pyautogui as pg
 import time 
 import pyperclip as pc
 
-teste = ['2022','2021','2321','7898','5645']
+teste = []
 
 time.sleep(3)
 
 for a in teste:
     
     pg.write(a) # numero da nota 
-    pg.press('enter')
-    pg.write('07705502221') # cpnj
-    pg.press('enter')
-    pg.write('123,32') # valor da nota 
-    pg.press('enter')
-    pg.write('02/02/2022') # data de emissão
-    pg.press('enter')
-    pg.write('testeltda123') # fornecedor 
-    pg.press('enter')
+    pg.press('tab')
+    pg.write('') # cpnj
+    pg.press('tab')
+    pg.write('') # valor da nota 
+    pg.press('tab')
+    pg.write('') # data de emissão
+    pg.press('tab')
+    pg.write('') # fornecedor 
+    pg.press('tab')
    # pg.press('tab')
-    pg.write('20/03/2022') # data de vencimento
-    pg.press('enter')
-    pg.write('Boleto') # metodo de pagamento 
-    pg.press('enter')
-    pg.write('padrao') #rateio
-    pg.press('enter')
-    pg.press('enter')
+    pg.write('') # data de vencimento
+    pg.press('tab')
+    pg.write('') # metodo de pagamento 
+    pg.press('tab')
+    pg.write('') #rateio
+   
+   
     
